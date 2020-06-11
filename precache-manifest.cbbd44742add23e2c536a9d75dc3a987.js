25271b945f9fdb13cb01b4a869071c9cb6163f1a
rehan284 <supermanrehan284@gmail.com>
2020-06-11 10:36:56 +0400

    Updates

---
+++ precache-manifest.cbbd44742add23e2c536a9d75dc3a987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a5cb5309e794dd86694e908cfb4b9b",
+    "url": "/DeployCOVID/index.html"
+  },
+  {
+    "revision": "902d6bdb432105bc7cba",
+    "url": "/DeployCOVID/static/css/main.e33c32ce.chunk.css"
+  },
+  {
+    "revision": "427c66f5ba8d2be23991",
+    "url": "/DeployCOVID/static/js/2.ccb83ea9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/DeployCOVID/static/js/2.ccb83ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902d6bdb432105bc7cba",
+    "url": "/DeployCOVID/static/js/main.f1381c77.chunk.js"
+  },
+  {
+    "revision": "bea2c56b016dee7d3a52",
+    "url": "/DeployCOVID/static/js/runtime-main.1820bb66.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/DeployCOVID/static/media/image.d7265326.png"
+  }
+]);
